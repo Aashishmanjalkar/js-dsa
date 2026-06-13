2953f510f4380e07473adc52beb897de2091d921
Ashish Manjalkar <manjalkarashish7@gmail.com>
2026-06-14 01:11:43 +0530

    chn

---
+++ stack.js
@@ -0,0 +1,71 @@
+class Node  {
+    constructor(value){
+       this.value = value;
+       this.next = null;
+    }
+}
+
+class Stack {
+    constructor() {
+        this.head = null
+        this.tail = null
+        this.length = 1
+    }
+
+    peek(){
+        return this.head
+    }
+
+    push(value){
+        let node = new Node(value)
+        if(this.length === 0){
+            this.head = node
+            this.tail = node
+        }else{
+            let pointer = this.head
+            this.head = node   
+            this.head.next = pointer
+        }
+        this.length++
+        return this
+    }
+
+    pop(){
+        let pointer = this.head.next
+        this.head = pointer
+        this.length--
+        return this
+    }
+}
+
+let stack = new Stack()
+console.log(stack.push(1))
+console.log(stack.push(4))
+console.log(stack.push(7))
+console.log(stack.pop()) 
+
+
+
+class stackArray{
+    constructor(){
+        this.array = []
+    }
+    peek(){
+        return this.array[this.array.length - 1]
+    }
+
+    push(value){
+        this.array.push(value)
+        return this
+    }
+
+    pop(){
+        return this.array.pop()
+    }
+}
+
+let stackArr = new stackArray()
+console.log(stackArr.push(1))
+console.log(stackArr.push(4))
+console.log(stackArr.push(7))
+console.log(stackArr.pop())
