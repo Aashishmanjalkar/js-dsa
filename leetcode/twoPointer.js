fb485991febf69c8f943621d678abd6645ceb6a5
Ashish Manjalkar <manjalkarashish7@gmail.com>
2026-07-05 19:07:44 +0530

    two pointer problem solved

--- leetcode/twoPointer.js
+++ leetcode/twoPointer.js
@@ -18,3 +18,69 @@ var maxArea = function(height) {
     }
     return max
 };
+
+//Brute force solution
+var removeDuplicatesBruteForce = function(nums) {
+    let k = 0
+    let hash = {}
+    for(let i = 0; i < nums.length; i++){
+       if (hash[nums[i]] !== undefined) {
+            hash[nums[i]] += 1;
+        } else {
+            hash[nums[i]] = 1;
+            nums[k] = nums[i]
+            k++;
+        }
+    }
+    return k
+};
+
+//Two pointer solution
+var removeDuplicates = function(nums) {
+    let k = 1
+    for(let i = 1; i < nums.length; i++){
+        if(nums[i] !== nums[k-1]){
+            nums[k] = nums[i]
+            k++
+        }
+    }
+    return k
+};
+
+//Brute force solution solved by me using recursion
+var isHappyBruteForce = function(n) {
+    let arr = String(n).split('')
+    if(arr.length === 1 && arr[0] == 1){
+        return true
+    }else if(arr.length === 1 && arr[0] != 1){
+        return false
+    }else{
+        while(n > 9){
+            let squareSum = 0
+            for(let num of arr){
+               squareSum += Math.pow(num , 2)
+            }
+            return isHappyBruteForce(squareSum)
+        }
+    }
+};
+
+var isHappy = function(n) {
+    var getNext = function(n) {
+        let totalSum = 0;
+        while (n > 0) {
+            let d = n % 10;  // Get the last digit i.e reminder when divided by 10 - 19/10 -> 9
+            n = Math.floor(n / 10); // Remove the last digit from n - 19/10 -> 1
+            totalSum += d * d;
+        } 
+        return totalSum;  
+    }
+
+    let slow = n
+    let fast = getNext(n)
+    while(fast !== 1 && slow !== fast){
+        slow = getNext(slow)
+        fast = getNext(getNext(fast))
+    }
+    return fast === 1;
+}
