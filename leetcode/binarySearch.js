b3daa926d4313e05cfdf2b45b0c9ec8c7d50014d
Ashish Manjalkar <manjalkarashish7@gmail.com>
2026-07-15 20:13:34 +0530

    updates

--- leetcode/binarySearch.js
+++ leetcode/binarySearch.js
@@ -64,3 +64,37 @@ var searchRotate = function(nums, target) {
 searchRotate([4,5,6,7,0,1,2], 0)
 // console.log(searchRotate([1,3], 3))
 // console.log(searchRotate([1], 0))
+
+
+var searchRange = function(nums, target) {
+    let leftIndex = binarySearch(nums, target, true);
+    if(leftIndex < 0){
+        return [-1,-1]
+    }
+    let rightIndex = binarySearch(nums, target, false);  
+    return [leftIndex, rightIndex]
+};
+
+function binarySearch(nums, target, isLeft){
+    let left = 0
+    let right = nums.length - 1
+    let targetIndex = -1
+    while(left <= right){ //Important Point Here to notice
+        let mid = Math.floor(left + (right - left)/2)
+        if(nums[mid] === target){
+            targetIndex = mid
+            if(isLeft){ // Important Point Here to notice
+                right = mid - 1 // Search for left index
+            }else{
+                left = mid + 1 // Search for right index
+            }
+        } else if (target > nums[mid]){
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    return targetIndex
+}
+
+searchRange([5,7,7,8,8,10], 8) //34. Find First and Last Position of Element in Sorted Array
