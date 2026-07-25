0d00e7940e2015a97f41028587347d7245c9aee7
Ashish Manjalkar <manjalkarashish7@gmail.com>
2026-07-25 17:12:24 +0530

    updates

--- leetcode/trees.js
+++ leetcode/trees.js
@@ -75,3 +75,28 @@ var inOrderTraversalRecursive = function(root) {
     traverse(root)
     return result
 }
+
+// Iterative approach to check if a tree is symmetric or not 
+var isSymmetric = function(root) {
+    if(!root) return true
+
+    const queue = [root.left, root.right]
+
+    while(queue.length){
+        const left = queue.shift()
+        const right = queue.shift()
+        
+        if(!left && !right) continue;
+
+        if(!left || !right) return false
+
+        if(left.val !== right.val) return false
+
+        queue.push(left.left)
+        queue.push(right.right) 
+        
+        queue.push(left.right)
+        queue.push(right.left) 
+    }
+    return true
+};
