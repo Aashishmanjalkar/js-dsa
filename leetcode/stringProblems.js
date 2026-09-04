d1934b28a30972c4c0467fc73962d315680cc95c
Ashish Manjalkar <manjalkarashish7@gmail.com>
2026-09-04 22:38:28 +0530

    Longest common prefix

--- leetcode/stringProblems.js
+++ leetcode/stringProblems.js
@@ -5,3 +5,20 @@ var isPalindrome = function(s) {
 };
 let s = "A man, a plan, a canal: Panama"
 console.log(isPalindrome(s))
+
+
+var longestCommonPrefix = function(strs) {
+    if(strs.length == 0 || strs[0] == "") return ""
+    let sortedArr = strs.sort()
+    let str = ''
+    for(let i = 0; i < sortedArr[0].length; i++){
+        if(sortedArr[0][i] ==  sortedArr[strs.length - 1][i]){
+            str += sortedArr[0][i]
+        }else{
+           break
+        }
+    }
+    return str
+};
+
+longestCommonPrefix(["flower","flow","flight"])
